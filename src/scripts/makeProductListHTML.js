195898b08f2c9371037709b6b34f24ea49dfc3ce
Connor Bailey <bailey.connor.p@gmail.com>
2019-02-08 16:00:46 -0600

    initial styling

--- src/scripts/makeProductListHTML.js
+++ src/scripts/makeProductListHTML.js
@@ -19,10 +19,18 @@ function makeProductListHTML() {
                     <h4 class="reviewHeader">Third Most Recent</h4>
                     <div class="productReview">${product.review3}</div>
                 </div>
+                <button id="startNewReview${product.id}" class="visible">Write A New Product Review!</button>
+                <section id="newReview${product.id}" class="hidden">
+                    <fieldset class="hidden">
+                        <label for="entry">Write Your New Review Here!</label>
+                        <textarea name="entry" id="newReviewText${product.id}" cols="30" rows="10"></textarea>
+                    </fieldset>
+                    <button id="saveNewReview${product.id}" class="hidden">SUBMIT REVIEW</button>
+                </section>
             </div>
             `
             }).join("")
-            console.log(productHTML)
+            // console.log(productHTML)
             return productHTML
         }))
 
